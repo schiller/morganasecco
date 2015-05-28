d22e306378480e41eab38fb1584346f2dc2a24c2
Luiz Gustavo Schiller <schillerbr@gmail.com>
2015-05-28 17:05:20 -0300

    Background img on home

--- client/templates/galleries/gallery-thumbnail.js
+++ client/templates/galleries/gallery-thumbnail.js
@@ -1,6 +1,6 @@
 Template.galleryThumbnail.helpers({
 	cover: function () {
-		var dummyImg = {title: "Não há fotos na galeria", url: "/img/no-pic.gif"};
+		var dummyImg = {title: "Não há fotos na galeria", url: "/images/no-pic.gif"};
 		return this.cover || Photos.findOne({galleryId : this._id}) || dummyImg;
 	}
 });
