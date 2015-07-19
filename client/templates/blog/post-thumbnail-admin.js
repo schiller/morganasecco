b106f7c7f09ed4e425c2c1d7dbda11c538c41394
Luiz Gustavo Schiller <schiller@Schillers-MacBook-Pro.local>
2015-07-18 21:47:17 -0300

    Updates Posts Admin page to new template

---
+++ client/templates/blog/post-thumbnail-admin.js
@@ -0,0 +1,6 @@
+Template.postThumbnailAdmin.helpers({
+  cover: function () {
+    var dummyImg = {title: "Não há fotos no post", url: "/images/morgana_coracao_preto.jpg"};
+    return Photos.findOne({_id : this.coverId}) || Photos.findOne({postId : this._id}) || dummyImg;
+  }
+});
