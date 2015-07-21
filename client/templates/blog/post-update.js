f071aeef84f1c96c3655e1502ed3a3c0111a7a9f
Luiz Gustavo Schiller <schiller@Schillers-MacBook-Pro.local>
2015-07-21 16:22:57 -0300

    Fixes portuguese error

--- client/templates/blog/post-update.js
+++ client/templates/blog/post-update.js
@@ -1,5 +1,14 @@
-Template.postUpdate.helpers({
-	gallery: function () {
-		return Galleries.findOne({_id: this.galleryId});
+Template.postUpdate.events({
+	'click #delete-post-btn': function () {
+		var confirmDelete = confirm("Tem certeza que deseja deletar o post e todas suas fotos? Esta ação é irreversível.");
+
+		if (confirmDelete === true) {
+			Meteor.call("removePhotos", this._id);
+			Posts.remove(this._id);
+
+			Router.go('postsAdmin');
+		}
+
+		return false;
 	}
 });
