2afe5f83f15f99506c3c67bb6dfc9218c3854599
Luiz Gustavo Schiller <schiller@Schillers-MacBook-Pro.local>
2015-08-04 15:57:46 -0300

    Adds sitemaps and robots.txt

--- client/templates/labs/labs.js
+++
@@ -1,9 +0,0 @@
-Template.labs.onRendered(function () {
-	function multisortable_stop (e) {
-		console.log("I've been sorted.");
-	}
-
-	this.$("ul").multisortable({
-		stop: multisortable_stop
-	});
-});
---
+++ server/sitemaps.js
@@ -0,0 +1,53 @@
+sitemaps.add('/mw_AllPages_sitemap.xml', function() {
+	var homePosts = Posts.find({
+		published: true,
+		home: true 
+	}).fetch();
+
+	var homePhotosArray = [];
+
+	_.each(homePosts, function(post) {
+		var photos = Photos.find({postId: post._id}).fetch();
+
+		_.each(photos, function (photo) {
+			var photoData = {};
+			photoData.loc = photo.urlThumb,
+			photoData.caption = photo.title,
+			photoData.title = photo.title
+			homePhotosArray.push(photoData);
+		});
+	});
+
+	var out = [
+	{ page: '/', images: homePhotosArray},
+	{ page: '/contact'},
+	{ page: '/about'},
+	{ page: '/posts'}
+	];
+
+	var posts = Posts.find({
+		published: true,
+		home: { '$ne': true }
+	}).fetch();
+
+	_.each(posts, function(post) {
+		var photos = Photos.find({postId: post._id}).fetch();
+		var photosArray = [];
+
+		_.each(photos, function (photo) {
+			var photoData = {};
+			photoData.loc = photo.urlThumb,
+			photoData.caption = photo.title,
+			photoData.title = photo.title
+			photosArray.push(photoData);
+		});
+
+		out.push({
+			page: 'posts/' + post._id,
+			lastmod: post.updatedAt,
+			images: photosArray
+		});
+	});
+
+	return out;
+});
