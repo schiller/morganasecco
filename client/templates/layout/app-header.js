02d0d5a69bbd0bee720350b9356a8c0e559cc0f2
Luiz Gustavo Schiller <schiller@Schillers-MacBook-Pro.local>
2015-08-03 23:15:30 -0300

    Collapse mobile menu after link click

--- client/templates/layout/app-header.js
+++ client/templates/layout/app-header.js
@@ -1,65 +1,93 @@
 Template.appHeader.onRendered(function () {
-	$(".js-stick").sticky({
-		topSpacing: 0
-	});
+	var self = this;
 
+	//sticky nav
+	var $jsStick = $(".js-stick");
+	$jsStick.sticky({topSpacing: 0});
+
+
+	//small height navbar
+	var $mainNav = $(".main-nav");
+	
 	$(window).scroll(function () {
 		if ($(window).scrollTop() > 10) {
-			$(".main-nav").removeClass("transparent");
+			$mainNav.removeClass("transparent");
 			$(".main-nav, .nav-logo-wrap .logo, .mobile-nav").addClass("small-height");
-		}
-		else {
-			$(".main-nav").addClass("transparent");
+		} else {
+			$mainNav.addClass("transparent");
 			$(".main-nav, .nav-logo-wrap .logo, .mobile-nav").removeClass("small-height");
 		}
 	});
 
-	var mnHasSub = $(".mn-has-sub");
-	var mnThisLi;
 
-	$(".mobile-on .mn-has-sub").find(".fa:first").removeClass("fa-angle-right").addClass("fa-angle-down");
+	//Inner Li mobile
+	var $mobileNav = $(".mobile-nav");
 
-	mnHasSub.on('click', function () {
-		if($(".mobile-nav").is(":visible")) {
+	function toggleInnerLi () {
+		if($mobileNav.is(":visible")) {
 			var mnThisLi = $(this).parent("li:first");
 			if (mnThisLi.hasClass("js-opened")) {
 				mnThisLi.find(".mn-sub:first").slideUp(function(){
 					mnThisLi.removeClass("js-opened");
 					mnThisLi.find(".mn-has-sub").find(".fa:first").removeClass("fa-angle-up").addClass("fa-angle-down");
 				});
-			}
-			else {
+			} else {
 				$(this).find(".fa:first").removeClass("fa-angle-down").addClass("fa-angle-up");
 				mnThisLi.addClass("js-opened");
 				mnThisLi.find(".mn-sub:first").slideDown();
 			}
 		}
-	});
+	}	
 
-	mnThisLi = mnHasSub.parent("li");
+	$(".mobile-on .mn-has-sub")
+	.find(".fa:first")
+	.removeClass("fa-angle-right")
+	.addClass("fa-angle-down");	
+
+	var $mnHasSub = $(".mn-has-sub");
+	$mnHasSub.on('click', toggleInnerLi);
+
+
+	//desktop nav dropdowns
+	var mnThisLi = $mnHasSub.parent("li");
 	mnThisLi.hover(function () {
-		if (!$(".mobile-nav").is(":visible")) {
+		if (!$mobileNav.is(":visible")) {
 			$(this).find(".mn-sub:first").stop(true, true).fadeIn("fast");
 		}
 	}, function () {
-		if (!$(".mobile-nav").is(":visible")) {
+		if (!$mobileNav.is(":visible")) {
 			$(this).find(".mn-sub:first").stop(true, true).delay(100).fadeOut("fast");
 		}
 	});
 
-	$('.mobile-nav').on('click', function () {
-		if ($(".desktop-nav").hasClass("js-opened")) {
-			$(".desktop-nav").slideUp("slow", "easeOutExpo", function () {
-				$(".desktop-nav").removeAttr("style");
+
+	//toggle mobile navbar
+	function toggleNavbar () {
+		if ($desktopNav.hasClass("js-opened")) {
+			$desktopNav.slideUp("slow", "easeOutExpo", function () {
+				$desktopNav.removeAttr("style");
 			}).removeClass("js-opened");
 			$(this).removeClass("active");
-		}
-		else {
-			$(".desktop-nav").slideDown("slow", "easeOutQuart").addClass("js-opened");
+		} else {
+			$desktopNav.slideDown("slow", "easeOutQuart").addClass("js-opened");
 			$(this).addClass("active");
 		}
+
 		return false;
-	});  
+	}
+
+	var $desktopNav = $(".desktop-nav");
+	$mobileNav.on('click', toggleNavbar);
+
+	// var $mobileNavbarLinks = $('.desktop-nav a:not(.mn-has-sub)');
+	// $mobileNavbarLinks.on('click', toggleNavbar);
+
+	Tracker.autorun(function () {
+		var count = Posts.find({featured: true, published: true}).count();
+		$mobileNavbarLinks = $('.desktop-nav a:not(.mn-has-sub)');
+		$mobileNavbarLinks.off('mouseup', toggleNavbar);
+		$mobileNavbarLinks.on('mouseup', toggleNavbar);
+	});	
 });
 
 Template.appHeader.helpers({
