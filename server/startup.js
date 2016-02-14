299aff57d145b9157e4f9edc322b60e46744acc8
schiller <schillerbr@gmail.com>
2016-02-14 19:11:51 -0200

    Removes db startup script

--- server/startup.js
+++ server/startup.js
@@ -1,15 +1,15 @@
-Meteor.startup(function () {
-  var photos = Photos.find().fetch();
+// Meteor.startup(function () {
+//   var photos = Photos.find().fetch();
 
-  _.each(photos, function (photo) {
-    Photos.update(
-      {_id: photo._id},
-      {$set: {
-        urlVeryLarge: photo.urlLarge,
-        urlMedium: photo.urlThumb,
-        urlSmall: photo.urlThumb,
-        urlMicro: photo.urlThumb
-      }}
-    );
-  });
-});
+//   _.each(photos, function (photo) {
+//     Photos.update(
+//       {_id: photo._id},
+//       {$set: {
+//         urlVeryLarge: photo.urlLarge,
+//         urlMedium: photo.urlThumb,
+//         urlSmall: photo.urlThumb,
+//         urlMicro: photo.urlThumb
+//       }}
+//     );
+//   });
+// });
