dbffaca634efd2e30881ca4d5a1c2f8dc20b1ce7
schiller <schillerbr@gmail.com>
2016-02-15 11:01:48 -0200

    Adds phone and subject on contact form

--- server/email/email.js
+++ server/email/email.js
@@ -3,12 +3,16 @@ Meteor.methods({
     // Important server-side check for security and data integrity
     check(doc, Schemas.Contact);
 
-    var subject = doc.subject || 'Website Contact Form - Message From ' + doc.name;
+    var subject = doc.subject || 'Mensagem de ' + doc.name;
+    var phone = doc.phone || '-';
 
     var emailTo = Meteor.settings.ContactEmail || 'contato@morganasecco.com.br';
 
     // Build the e-mail text
-    var text = 'Name: ' + doc.name + '\n\n' + 'Email: ' + doc.email + '\n\n\n\n' + doc.message;
+    var text = 'Nome: ' + doc.name + '\n\n' + 
+      'Telefone: ' + phone + '\n\n' + 
+      'Email: ' + doc.email + '\n\n\n\n' + 
+      doc.message;
 
     this.unblock();
 
